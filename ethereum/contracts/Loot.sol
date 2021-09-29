a1149fb6a82ab45189b9820dec245ca5dcb4edbe
jacmos3 <j.moscioni@semproxlab.it>
2021-09-29 13:27:26 +0200

    added little traveler implementation

--- ethereum/contracts/Loot.sol
+++ ethereum/contracts/Loot.sol
@@ -1286,235 +1286,187 @@ abstract contract ERC721Enumerable is ERC721, IERC721Enumerable {
 
 contract Loot is ERC721Enumerable, ReentrancyGuard, Ownable {
 
-        string[] private weapons = [
-        "Warhammer",
-        "Quarterstaff",
-        "Maul",
-        "Mace",
-        "Club",
-        "Katana",
-        "Falchion",
-        "Scimitar",
-        "Long Sword",
-        "Short Sword",
-        "Ghost Wand",
-        "Grave Wand",
-        "Bone Wand",
-        "Wand",
-        "Grimoire",
-        "Chronicle",
-        "Tome",
-        "Book"
+       //contract TheLittleTraveler is ERC721Enumerable, ReentrancyGuard, Ownable {
+
+  // ** //
+    uint256 public tokenCounter=1;
+
+        //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+        string[] private place = [
+        "Rome",
+        "London",
+        "Paris",
+        "Bangkok",
+        "Martellago",
+        "Marrakesh",
+        "Shanghai",
+        "Florence",
+        "Barcelona",
+        "Munich",
+        "Bali",
+        "Lagos",
+        "Oslo",
+        "Moscow",
+        "Amsterdam",
+        "Buenos Aires",
+        "Fortaleza",
+        "Istanbul"
+    ];
+    //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+    string[] private character = [
+        "Kind",
+        "Assertive",
+        "Defensive",
+        "Imaginative",
+        "Empathic",
+        "Dynamic",
+        "Introvert",
+        "Vulnerable",
+        "Kind",
+        "Lovely",
+        "Conscientious",
+        "Curious"
+    ];
+    //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+    string[] private transport = [
+        "Train",
+        "Car",
+        "Hitch-hiking",
+        "Airplane",
+        "Cruise",
+        "4 wheel drive car",
+        "Bus",
+        "Airplane",
+        "RickShaw",
+        "Apecar",
+        "Vespa",
+        "Horse carriage",
+        "Bicycle",
+        "Motorbike",
+        "Campervan"
+    ];
+
+
+    //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+    string[] private language = [
+        "Italian",
+        "French",
+        "German",
+        "Spanish",
+        "Portuguese",
+        "Swahili",
+        "Thai",
+        "Chinese",
+        "Russian",
+        "Hindi",
+        "Bulgarian",
+        "Japanese",
+        "Danish",
+        "Korean",
+        "Arab"
+    ];
+    //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+    string[] private mood = [
+        "Light hearted",
+        "Serene",
+        "Worried",
+        "In love",
+        "Festive",
+        "Energetic",
+        "Sleepy",
+        "Quiet",
+        "Anxious"
+
+
+    ];
+    //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+    string[] private occupation = [
+        "TripsCommunity Member",
+        "SmartWorker",
+        "Host",
+        "Traveller",
+        "Service Provider",
+        "Digital Nomad",
+        "Freelancer",
+        "Unemployed",
+        "Crypto Trader",
+        "DeFi Airdrop Hunter",
+        "DAO Community Manager",
+        "DAO Member",
+        "NFT flipper",
+        "NFT collector",
+        "Tech Lover"
+    ];
+   
+    //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+    string[] private accomodation = [
+        "Hotel",
+        "Apartment",
+        "Hostel"
     ];
     
-    string[] private chestArmor = [
-        "Divine Robe",
-        "Silk Robe",
-        "Linen Robe",
-        "Robe",
-        "Shirt",
-        "Demon Husk",
-        "Dragonskin Armor",
-        "Studded Leather Armor",
-        "Hard Leather Armor",
-        "Leather Armor",
-        "Holy Chestplate",
-        "Ornate Chestplate",
-        "Plate Mail",
-        "Chain Mail",
-        "Ring Mail"
+    //IL NUMERO DEGLI ELEMENTI DEVE ESSERE UN MULTIPLO DI 3
+    string[] private bag = [
+        "Pen",
+        "Swiss knife",
+        "Mobile phone",
+        "Notebook ",
+        "Digital Camera",
+        "Knife"
     ];
-    
-    string[] private headArmor = [
-        "Ancient Helm",
-        "Ornate Helm",
-        "Great Helm",
-        "Full Helm",
-        "Helm",
-        "Demon Crown",
-        "Dragon's Crown",
-        "War Cap",
-        "Leather Cap",
-        "Cap",
-        "Crown",
-        "Divine Hood",
-        "Silk Hood",
-        "Linen Hood",
-        "Hood"
-    ];
-    
-    string[] private waistArmor = [
-        "Ornate Belt",
-        "War Belt",
-        "Plated Belt",
-        "Mesh Belt",
-        "Heavy Belt",
-        "Demonhide Belt",
-        "Dragonskin Belt",
-        "Studded Leather Belt",
-        "Hard Leather Belt",
-        "Leather Belt",
-        "Brightsilk Sash",
-        "Silk Sash",
-        "Wool Sash",
-        "Linen Sash",
-        "Sash"
-    ];
-    
-    string[] private footArmor = [
-        "Holy Greaves",
-        "Ornate Greaves",
-        "Greaves",
-        "Chain Boots",
-        "Heavy Boots",
-        "Demonhide Boots",
-        "Dragonskin Boots",
-        "Studded Leather Boots",
-        "Hard Leather Boots",
-        "Leather Boots",
-        "Divine Slippers",
-        "Silk Slippers",
-        "Wool Shoes",
-        "Linen Shoes",
-        "Shoes"
-    ];
-    
-    string[] private handArmor = [
-        "Holy Gauntlets",
-        "Ornate Gauntlets",
-        "Gauntlets",
-        "Chain Gloves",
-        "Heavy Gloves",
-        "Demon's Hands",
-        "Dragonskin Gloves",
-        "Studded Leather Gloves",
-        "Hard Leather Gloves",
-        "Leather Gloves",
-        "Divine Gloves",
-        "Silk Gloves",
-        "Wool Gloves",
-        "Linen Gloves",
-        "Gloves"
-    ];
-    
-    string[] private necklaces = [
-        "Necklace",
-        "Amulet",
-        "Pendant"
-    ];
-    
-    string[] private rings = [
-        "Gold Ring",
-        "Silver Ring",
-        "Bronze Ring",
-        "Platinum Ring",
-        "Titanium Ring"
-    ];
-    
-    string[] private suffixes = [
-        "of Power",
-        "of Giants",
-        "of Titans",
-        "of Skill",
-        "of Perfection",
-        "of Brilliance",
-        "of Enlightenment",
-        "of Protection",
-        "of Anger",
-        "of Rage",
-        "of Fury",
-        "of Vitriol",
-        "of the Fox",
-        "of Detection",
-        "of Reflection",
-        "of the Twins"
-    ];
-    
-    string[] private namePrefixes = [
-        "Agony", "Apocalypse", "Armageddon", "Beast", "Behemoth", "Blight", "Blood", "Bramble", 
-        "Brimstone", "Brood", "Carrion", "Cataclysm", "Chimeric", "Corpse", "Corruption", "Damnation", 
-        "Death", "Demon", "Dire", "Dragon", "Dread", "Doom", "Dusk", "Eagle", "Empyrean", "Fate", "Foe", 
-        "Gale", "Ghoul", "Gloom", "Glyph", "Golem", "Grim", "Hate", "Havoc", "Honour", "Horror", "Hypnotic", 
-        "Kraken", "Loath", "Maelstrom", "Mind", "Miracle", "Morbid", "Oblivion", "Onslaught", "Pain", 
-        "Pandemonium", "Phoenix", "Plague", "Rage", "Rapture", "Rune", "Skull", "Sol", "Soul", "Sorrow", 
-        "Spirit", "Storm", "Tempest", "Torment", "Vengeance", "Victory", "Viper", "Vortex", "Woe", "Wrath",
-        "Light's", "Shimmering"  
-    ];
-    
-    string[] private nameSuffixes = [
-        "Bane",
-        "Root",
-        "Bite",
-        "Song",
-        "Roar",
-        "Grasp",
-        "Instrument",
-        "Glow",
-        "Bender",
-        "Shadow",
-        "Whisper",
-        "Shout",
-        "Growl",
-        "Tear",
-        "Peak",
-        "Form",
-        "Sun",
-        "Moon"
-    ];
-    
+
     function random(string memory input) internal pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(input)));
     }
-    
-    function getWeapon(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "WEAPON", weapons);
-    }
-    
-    function getChest(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "CHEST", chestArmor);
-    }
-    
-    function getHead(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "HEAD", headArmor);
-    }
-    
-    function getWaist(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "WAIST", waistArmor);
+
+    function extractOutput(string[] memory sourceArray, uint8 toHoundred) internal view returns (string memory){
+        uint8 delta = toHoundred > 95 ? 1 : toHoundred > 75 ? 2 : 3;
+        uint8 len = uint8(sourceArray.length);
+        uint8 x = len / 3;
+        uint8 min = len - (delta* x);
+        uint8 max = (len -1) - ((delta -1) * x);
+        //estraggo un numero a caso incluso tra min e max
+        uint8 rand = uint8((random(string(abi.encodePacked(block.difficulty, msg.sender, toHoundred,block.timestamp))) % (max-min)) + min);
+        return sourceArray[rand];
     }
 
-    function getFoot(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "FOOT", footArmor);
+    function getPlace(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "PLACE", place);
     }
-    
-    function getHand(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "HAND", handArmor);
+
+    function getCharacter(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "CHARACTER", character);
     }
-    
-    function getNeck(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "NECK", necklaces);
+
+    function getTransport(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "TRANSPORT", transport);
     }
-    
-    function getRing(uint256 tokenId) public view returns (string memory) {
-        return pluck(tokenId, "RING", rings);
+
+    function getLanguage(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "LANGUAGE", language);
     }
-    
+
+    function getMood(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "MOOD", mood);
+    }
+
+    function getOccupation(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "OCCUPATION", occupation);
+    }
+
+    function getAccomodation(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "ACCOMODATION", accomodation);
+    }
+
+    function getBag(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "BAG", bag);
+    }
+
     function pluck(uint256 tokenId, string memory keyPrefix, string[] memory sourceArray) internal view returns (string memory) {
         uint256 rand = random(string(abi.encodePacked(keyPrefix, toString(tokenId))));
-        string memory output = sourceArray[rand % sourceArray.length];
-        uint256 greatness = rand % 21;
-        if (greatness > 14) {
-            output = string(abi.encodePacked(output, " ", suffixes[rand % suffixes.length]));
-        }
-        if (greatness >= 19) {
-            string[2] memory name;
-            name[0] = namePrefixes[rand % namePrefixes.length];
-            name[1] = nameSuffixes[rand % nameSuffixes.length];
-            if (greatness == 19) {
-                output = string(abi.encodePacked('"', name[0], ' ', name[1], '" ', output));
-            } else {
-                output = string(abi.encodePacked('"', name[0], ' ', name[1], '" ', output, " +1"));
-            }
-        }
+        uint8 toHoundred = uint8(rand % 100);
+        string memory output = extractOutput(sourceArray,toHoundred);
+
         return output;
     }
 
@@ -1522,41 +1474,41 @@ contract Loot is ERC721Enumerable, ReentrancyGuard, Ownable {
         string[17] memory parts;
         parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
 
-        parts[1] = getWeapon(tokenId);
+        parts[1] = getPlace(tokenId);
 
         parts[2] = '</text><text x="10" y="40" class="base">';
 
-        parts[3] = getChest(tokenId);
+        parts[3] = getCharacter(tokenId);
 
         parts[4] = '</text><text x="10" y="60" class="base">';
 
-        parts[5] = getHead(tokenId);
+        parts[5] = getTransport(tokenId);
 
         parts[6] = '</text><text x="10" y="80" class="base">';
 
-        parts[7] = getWaist(tokenId);
+        parts[7] = getLanguage(tokenId);
 
         parts[8] = '</text><text x="10" y="100" class="base">';
 
-        parts[9] = getFoot(tokenId);
+        parts[9] = getMood(tokenId);
 
         parts[10] = '</text><text x="10" y="120" class="base">';
 
-        parts[11] = getHand(tokenId);
+        parts[11] = getOccupation(tokenId);
 
         parts[12] = '</text><text x="10" y="140" class="base">';
 
-        parts[13] = getNeck(tokenId);
+        parts[13] = getAccomodation(tokenId);
 
         parts[14] = '</text><text x="10" y="160" class="base">';
 
-        parts[15] = getRing(tokenId);
+        parts[15] = getBag(tokenId);
 
         parts[16] = '</text></svg>';
 
         string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8]));
         output = string(abi.encodePacked(output, parts[9], parts[10], parts[11], parts[12], parts[13], parts[14], parts[15], parts[16]));
-        
+
         string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Bag #', toString(tokenId), '", "description": "Loot is randomized adventurer gear generated and stored on chain. Stats, images, and other functionality are intentionally omitted for others to interpret. Feel free to use Loot in any way you want.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
@@ -1567,12 +1519,12 @@ contract Loot is ERC721Enumerable, ReentrancyGuard, Ownable {
         require(tokenId > 0 && tokenId < 7778, "Token ID invalid");
         _safeMint(_msgSender(), tokenId);
     }
-    
+
     function ownerClaim(uint256 tokenId) public nonReentrant onlyOwner {
         require(tokenId > 7777 && tokenId < 8001, "Token ID invalid");
         _safeMint(owner(), tokenId);
     }
-    
+
     function toString(uint256 value) internal pure returns (string memory) {
     // Inspired by OraclizeAPI's implementation - MIT license
     // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
@@ -1594,7 +1546,7 @@ contract Loot is ERC721Enumerable, ReentrancyGuard, Ownable {
         }
         return string(buffer);
     }
-    
+
     constructor() ERC721("Loot", "LOOT") Ownable() {}
 }
 
