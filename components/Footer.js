8b4e6c49a7d0c405b9e84a8b194ee1376fecfe03
Luca <34341206+lucadgtrip@users.noreply.github.com>
2021-12-13 13:59:12 +0200

    footer text

--- components/Footer.js
+++ components/Footer.js
@@ -22,13 +22,13 @@ function Footer() {
         </a>{" "}
         and licensed under MIT license.
         <br />
-        Final Traveler Loot NFTs are CC0 Public Domain
+        Traveler Loot modifications and NFTs are CC0 Public Domain
         <div className="flex py-8 w-full justify-center space-x-6">
           <Image className="text-white mx-2" src="https://i.creativecommons.org/p/zero/1.0/88x31.png" />
         </div>
-        Project is insipired by <a target= "_blank" href="https://www.lootproject.com/"> Project Loot</a>
+        Inspired by <a target= "_blank" href="https://www.lootproject.com/"> Project Loot</a>
         {" "}
-        and edited by <a target="_blank" href="https://www.tripscommunity.com">Trips Community</a>
+        and forked by <a target="_blank" href="https://www.tripscommunity.com">Trips Community</a>
 
       </div>
     </div>
