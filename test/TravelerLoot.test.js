3e92cb0bf7adf35f3c35de4e495f36f0329ef866
Jacopo <jacopogithub.pbcc5@aleeas.com>
2021-12-19 04:00:12 +0100

    added other tests

--- test/TravelerLoot.test.js
+++ test/TravelerLoot.test.js
@@ -27,14 +27,48 @@ describe('TravelerLoot', ()=>{
   });
 
 
-  it ('owner can claim a Traveler Loot', async ()=>{
+  it ('owner can claim a Traveler Loot in range #2001-#2222', async ()=>{
     const hash = await travelerLoot.methods.claimForOwner("2001").send({from: accounts[0],gas: 30000000, gasPrice: 1000000});
     console.log(hash.transactionHash);
-      assert.ok(hash.transactionHash);
+    assert.ok(hash.transactionHash);
   });
 
   it ('can download token uri', async () => {
     const tokenURI = await travelerLoot.methods.tokenURI("2001").call();
     assert.ok(tokenURI);
   });
+
+  it ('owner cant claim a Traveler Loot in range #2223-#10000 range', async ()=>{
+    try{
+    const hash = await travelerLoot.methods.claimForOwner("2223").send({from: accounts[0],gas: 30000000, gasPrice: 1000000});
+    }
+    catch(e){
+      console.log(e.message);
+      assert.ok(true);
+      return;
+    }
+    assert.ok(false,"owner has minted");
+  });
+
+  it ('not owner nor users can claim a TL > #10000 range', async ()=>{
+    try{
+      const hash = await travelerLoot.methods.claimForOwner("11111").send({from: accounts[0],gas: 30000000, gasPrice: 1000000});
+    }
+    catch(e){
+      console.log(e.message);
+      assert.ok(true);
+      try{
+        const hash2 = await travelerLoot.methods.claimForOwner("222222").send({from: accounts[1],gas: 30000000, gasPrice: 1000000});
+      }
+      catch(err){
+        console.log(err.message);
+        assert.ok(true);
+        return;
+      }
+      return;
+    }
+    assert.ok(false,"owner or user has minted");
+  });
+
+
 })
