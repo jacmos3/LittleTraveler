e0b85c48fe830e16ba0ee32eb9fabc4c50683df0
Jacopo <jacopogithub.pbcc5@aleeas.com>
2022-02-08 03:15:57 +0100

    indentation

--- components/IndexSections/Video.js
+++ components/IndexSections/Video.js
@@ -14,22 +14,16 @@ class Video extends Component{
             <span className="uppercase sm:text-xl tracking-widest"></span>
             <h2 className="text-center mt-4 capitalize text-trips-1">An NFT bridge With The Travel World</h2>
             <br />
-
             <Embed
               id='7wlySD-fk_Q'
               placeholder='/meta.png'
               source='youtube'
             />
-
             <p className="text-xl ">
-
-            <br />
-
+              <br />
                 We want to support the traditional travel industry in its journey towards Web3.
                 <br />
-
-               See it as a cooperation between the crypto travel world and the travel industry.
-
+                See it as a cooperation between the crypto travel world and the travel industry.
                 <br />
                 <a href="https://medium.com/trips-community/the-traveler-loot-the-offers-7fd8a4eb2410" target="_blank">🔗See the offers</a>
                 <br />
