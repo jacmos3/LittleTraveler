440645102a2a34d577c42951482033a7d2499aa9
Jacopo <jacopogithub.pbcc5@aleeas.com>
2022-02-21 00:44:01 +0100

    refactoring input details

--- components/IndexSections/Gallery.js
+++ components/IndexSections/Gallery.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import styles from "../../styles/components/Gallery.module.scss";
+import {galleryDetails} from "../../public/lists/galleryDetails.js"
 
 class Gallery extends Component {
     constructor(props) {
@@ -8,28 +9,7 @@ class Gallery extends Component {
 
 
     render() {
-        var images = [
-            {key: "1", image: 'img/littletravelers/church-441956-lt.png'},
-            {key: "2", image: 'img/littletravelers/mountain-1862077-lt.png'},
-            {key: "3", image: 'img/littletravelers/petronas-twin-towers-at-night-kuala-lumpur-malaysia-lt.png'},
-            {key: "4", image: 'img/littletravelers/pexels-dlkr-5493279-lt.png'},
-            {key: "5", image: 'img/littletravelers/types/Little-Traveler-1.png'},
-            {key: "6", image: 'img/littletravelers/types/Little-Traveler-2.png'},
-            {key: "7", image: 'img/littletravelers/pexels-shahbaz-zaman-4609844-lt.png'},
-            {key: "8", image: 'img/littletravelers/danyu-wang-sR7_ImYvt1Q-lt.png'},
-            {key: "9", image: 'img/littletravelers/robot-1464596-lt.png'},
-            {key: "10", image: 'img/littletravelers/future-3716486-lt.png'},
-            {key: "11", image: 'img/littletravelers/types/Little-Traveler-3.png'},
-            {key: "12", image: 'img/littletravelers/types/Little-Traveler-5.png'},
 
-            /*  {image:'img/littletravelers/types/Little-Traveler-4.png'},
-              {image:'img/littletravelers/types/Little-Traveler-7.png'},
-              {image:'img/littletravelers/types/Little-Traveler-8.png'},
-              {image:'img/littletravelers/types/Little-Traveler-9.png'},
-
-              {image:'img/littletravelers/types/Little-Traveler-10.png'},
-              {image:'img/littletravelers/types/Little-Traveler-12.png'}*/
-        ]
 
         return (
             <div className={`${styles.gallery__section} flex justify-around`}>
@@ -37,7 +17,7 @@ class Gallery extends Component {
                     <h2 className="text-center mt-4 capitalize text-trips-1">An Incredible Variety of Travelers</h2>
                     <div className={`${styles.image__container}`}>
                         {
-                            images.map(el =>
+                            galleryDetails.map(el =>
                                 <div className={`${styles.image}`} key={el.key}>
                                     <img loading="lazy" src={el.image}/>
                                     <img loading="lazy" src={el.image}/>
